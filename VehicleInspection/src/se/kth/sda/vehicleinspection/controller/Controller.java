adba2cb0f0a90fa411662929a92d37862901b5f6
guddi22 <vimalajayaprakash@gmail.com>
2017-02-22 13:06:26 +0100

    Update Controller.java

--- VehicleInspection/src/se/kth/sda/vehicleinspection/controller/Controller.java
+++ VehicleInspection/src/se/kth/sda/vehicleinspection/controller/Controller.java
@@ -17,11 +17,19 @@ public class Controller {
 
     private Garage garage;
     
+    /**
+     * Controller Constructor 
+     * Object of Garage class created.
+     */
     public Controller() {
         garage = new Garage();
         System.out.println("Open garage door");
     }
 
+    /**
+     * This method helps us to move Vehicle to Garage 
+     * @return Garage details
+     */
     public Garage moveVehicleToGarage() {
         int id = garage.nextCustomer();
         System.out.println("Now serving customer " + id);
@@ -30,17 +38,38 @@ public class Controller {
         return garage;
     }
 
-    public void ensureVehicleExistsInRegistry(VehicleDTO vehicleDTO,
-            CustomerDTO customerDTO) {
-        VehicleRegistry.ensureVehicleExistsInRegistry(vehicleDTO, customerDTO);
+    /**
+     * This method is Check if the Vehicle Number exist in Registry
+     * @param vehicleDTO - Contain Vehicle information received from customer
+     * @param customerDTO - Contain Customer information received.
+     */
+    public void ensureVehicleExistsInRegistry(VehicleDTO vehicleDTO,CustomerDTO customerDTO) throws Exception {
+        try{
+            VehicleRegistry.ensureVehicleExistsInRegistry(vehicleDTO, customerDTO);
+        }
+        catch( Exception e)
+        {
+            throw e;
+        }
     }
 
+    /**
+     * This method call Recommendation Inspection class to fetch Recommended Inspection
+     * for given Vehicle Details
+     * @param vehicleDTO - Contains Vehicle information
+     * @return - Returns RecommendedInspection details.
+     */
     public RecommendedInspection getRecommendedInspection(VehicleDTO vehicleDTO) {
         RecommendedInspection r = RecommendedInspection.getRecommendedInspections(vehicleDTO);
         r.print();
         return r;
     }
 
+    /**
+     * This method computes Inspection Cost for the given Recommended Inspection
+     * @param recommendedInspection - Contains RecommendedInspection Details
+     * @return - Returns Cost of Inspection in Float.
+     */
     public Float computeInspectionCost(RecommendedInspection recommendedInspection) {
         InspectionDTO inspection = new InspectionDTO();
         Float totalCost = 0f;
@@ -51,16 +80,32 @@ public class Controller {
         return totalCost;
     }
 
+    /**
+     * This method Performs the Payment of the Inspection Cost
+     * @param p - Contains Payment details
+     */
     public void pay(Payment p) {
         p.performTransaction();
     }
 
+    /**
+     * This method Prints the Payment Receipt
+     * @param p - Contains Payment Details
+     * @param vehicleDTO - Contains Vehicle Details
+     * @param customerDTO - Contains Customer Details
+     */
     public void printPaymentReceipt(Payment p, VehicleDTO vehicleDTO, CustomerDTO customerDTO) {
         System.out.println("Printing payment receipt");
         PaymentReceipt paymentReceipt = new PaymentReceipt(p, vehicleDTO, customerDTO);
         System.out.println(paymentReceipt.generatePaymentReceipt());
     }
 
+    /**
+     *  This method performs the Inspection based on Recommended Inspection
+     * @param vehicleDTO - Contains Vehicle Details
+     * @param recommendedInspection - Contains Recommended Inspection
+     * @return - Return the Inspected Result of the Inspection.
+     */
     public InspectionResult inspectVehicle(VehicleDTO vehicleDTO, RecommendedInspection recommendedInspection) {
         System.out.println("Performing Inspection");
         VehicleInspection vehicleInspection = new VehicleInspection(vehicleDTO, recommendedInspection);
@@ -68,11 +113,20 @@ public class Controller {
         return res;
     }
     
+    /**
+     * This method Prints the Inspection Receipt or Results.
+     * @param result - Contain the Inspection Results.
+     * @param customer - Contains the Customer Details.
+     */
     public void printInspectionReceipt(InspectionResult result, CustomerDTO customer){
         InspectionReceipt receipt = new InspectionReceipt(result, customer);
         receipt.generateReceipt();
     }
 
+    /**
+     *  This is to move the Inspected Vehicle Out of Garage after Completion of 
+     * Inspection.
+     */
     public void moveVehicleOutOfGarage() {
         System.out.println("Inspection process completed. Garage door closed.");
         garage.openDoor();
