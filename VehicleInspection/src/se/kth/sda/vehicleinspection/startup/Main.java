57463a69866e6f12c5f9e3c38e508717567a4cd2
guddi22 <vimalajayaprakash@gmail.com>
2017-02-22 13:09:45 +0100

    Update Main.java
    Exception Handling

--- VehicleInspection/src/se/kth/sda/vehicleinspection/startup/Main.java
+++ VehicleInspection/src/se/kth/sda/vehicleinspection/startup/Main.java
@@ -24,50 +24,65 @@ import se.kth.sda.vehicleinspection.model.RecommendedInspection;
  */
 public class Main {
 
-    public static void main(String args[]) {
-        VehicleDTO vehicleDTO1 = new VehicleDTO("AOH 5324", "Car", "VOLVO", "SUV", "BLACK");
-        VehicleDTO vehicleDTO2 = new VehicleDTO("CXB 2345", "Car", "SWIFT", "Compact", "RED");
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String args[]) throws Exception{
+        try{
+                VehicleDTO vehicleDTO1 = new VehicleDTO("AOH 5324", "Car", "VOLVO", "SUV", "BLACK");
+                VehicleDTO vehicleDTO2 = new VehicleDTO("CXB 2345", "Car", "SWIFT", "Compact", "RED");
 
-        AddressDTO addressDTO = new AddressDTO("Vidangsvage", "Stockholm", "16735", "Stockhlm", "Sweden");
+                AddressDTO addressDTO = new AddressDTO("Vidangsvage", "Stockholm", "16735", "Stockhlm", "Sweden");
 
-        CustomerDTO customerDTO = new CustomerDTO("CustPay 345LK", "Payal Gupta", addressDTO,
-                "9873645344", "user@yahoo.com");
+                CustomerDTO customerDTO = new CustomerDTO("CustPay 345LK", "Payal Gupta", addressDTO,
+                        "9873645344", "user@yahoo.com");
 
-        CreditCardDTO creditCardDTO = new CreditCardDTO("123456789123", "Sam", YearMonth.of(2018, Month.FEBRUARY),
-                "125", "1254");
+                CreditCardDTO creditCardDTO = new CreditCardDTO("123456789123", "Sam", YearMonth.of(2018, Month.FEBRUARY),
+                        "125", "1254");
 
-        Controller controller = new Controller();
-        controller.moveVehicleToGarage();  // TODO rename method
-        controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
-        RecommendedInspection recommendedInspection = controller.getRecommendedInspection(vehicleDTO1);
-        Float cost = controller.computeInspectionCost(recommendedInspection);
-        System.out.println("Pay by cash or credit card? You entered Cash");
-        Payment c = new CashPayment(cost, cost + 100);
-        controller.pay(c);
-        controller.printPaymentReceipt(c, vehicleDTO2, customerDTO);
-        InspectionResult result1 = controller.inspectVehicle(vehicleDTO1, recommendedInspection);
-        controller.printInspectionReceipt(result1, customerDTO);
+                Controller controller = new Controller();
+                controller.moveVehicleToGarage();  // TODO rename method
+                controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
+                RecommendedInspection recommendedInspection = controller.getRecommendedInspection(vehicleDTO1);
+                Float cost = controller.computeInspectionCost(recommendedInspection);
+                System.out.println("Pay by cash or credit card? You entered Cash");
+                Payment c = new CashPayment(cost, cost + 100);
+                controller.pay(c);
+                controller.printPaymentReceipt(c, vehicleDTO2, customerDTO);
+                InspectionResult result1 = controller.inspectVehicle(vehicleDTO1, recommendedInspection);
+                controller.printInspectionReceipt(result1, customerDTO);
 
-        controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
-        RecommendedInspection recommendedInspection2 = controller.getRecommendedInspection(vehicleDTO1);
-        controller.computeInspectionCost(recommendedInspection2);
-        System.out.println("Pay by cash or credit card? You entered credit card");
-        Payment cc = new CreditCardPayment(creditCardDTO, cost);
-        controller.pay(cc);
-        controller.printPaymentReceipt(cc, vehicleDTO2, customerDTO);
-        InspectionResult result2 = controller.inspectVehicle(vehicleDTO1, recommendedInspection2);
-        controller.printInspectionReceipt(result2, customerDTO);
+                controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
+                RecommendedInspection recommendedInspection2 = controller.getRecommendedInspection(vehicleDTO1);
+                controller.computeInspectionCost(recommendedInspection2);
+                System.out.println("Pay by cash or credit card? You entered credit card");
+                Payment cc = new CreditCardPayment(creditCardDTO, cost);
+                controller.pay(cc);
+                controller.printPaymentReceipt(cc, vehicleDTO2, customerDTO);
+                InspectionResult result2 = controller.inspectVehicle(vehicleDTO1, recommendedInspection2);
+                controller.printInspectionReceipt(result2, customerDTO);
 
-        /*controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
-        RecommendedInspection recommendedInspection3 = controller.getRecommendedInspection(vehicleDTO1);
-        controller.computeInspectionCost(recommendedInspection3);
-        controller.inspectVehicle(vehicleDTO1, recommendedInspection3);
-        controller.moveVehicleOutOfGarage();
+                /*controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
+                RecommendedInspection recommendedInspection3 = controller.getRecommendedInspection(vehicleDTO1);
+                controller.computeInspectionCost(recommendedInspection3);
+                controller.inspectVehicle(vehicleDTO1, recommendedInspection3);
+                controller.moveVehicleOutOfGarage();
 
-        controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
-        RecommendedInspection recommendedInspection4 = controller.getRecommendedInspection(vehicleDTO1);
-        controller.computeInspectionCost(recommendedInspection4);
-        controller.inspectVehicle(vehicleDTO1, recommendedInspection4);
-        controller.moveVehicleOutOfGarage();*/
+                controller.ensureVehicleExistsInRegistry(vehicleDTO1, customerDTO);
+                RecommendedInspection recommendedInspection4 = controller.getRecommendedInspection(vehicleDTO1);
+                controller.computeInspectionCost(recommendedInspection4);
+                controller.inspectVehicle(vehicleDTO1, recommendedInspection4);
+                controller.moveVehicleOutOfGarage();*/
+        }
+        catch( Exception e)
+        {
+           throw e;
+        }
+        finally{
+            System.out.println("Releasing resources");         
+        }
+
     }
 }
